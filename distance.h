4d7834d0de114863fff15259a1761657a74b3c08
Ericfengdc <43702827+Ericfengdc@users.noreply.github.com>
2019-04-24 15:42:19 +0800

    Update distance.h

--- distance.h
+++ distance.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdio.h>
 #include <math.h>
 #define PI                      3.1415926
@@ -14,63 +15,55 @@ double radian(double d)
 {
 	return d * PI / 180.0;   //角度1˚ = π / 180
 }
-/*
-double mysin(double x) 
-{
-	return (x - 1 / 6 *pow(x,3) + 1 / 120* pow(x,5) - 1 / 5040* pow(x,7));
-}
-double mycos(double x)
-{
-	return(1 - 1/2*pow(x,2)+1/24*pow(x,4)-1/720*pow(x,6)+1/40320*pow(x,8));
-}
-*/
-double mysin(double x) 
-{
-	float y = B * x + C * x * abs(x);
-	y = P * (y * abs(y) - y) + y;
-	return y;
-}
-double mycos(double x) 
-{
-	return mysin(x+PI/2);
-}
+
+double mysin(double x);
+double mycos(double x);
 //计算距离
-double get_distance(double lat1, double lng1, double lat2, double lng2)
-{
-	double radLat1 = radian(lat1);
-	double radLat2 = radian(lat2);
-	double a = radLat1 - radLat2;
-	double b = radian(lng1) - radian(lng2);
-	double dst = 2 * asin((sqrt(pow(mysin(a / 2), 2) + mycos(radLat1) * mycos(radLat2) * pow(mysin(b / 2), 2))));
-	//double dst = 2 * asin((sqrt(pow(sin(a / 2), 2) + cos(radLat1) * cos(radLat2) * pow(sin(b / 2), 2))));
+double get_distance(double lat1, double lng1, double lat2, double lng2);
+double distance(double lat1, double lng1, double lat2, double lng2);
+float get_diff_time(int a, int b); //a大b小
 
-	dst = dst * EARTH_RADIUS;
-	dst = round(dst * 10000) / 10000;
-	return dst;
-}
- double distance(double lat1,double lng1,double lat2,double lng2)
-{
+/*计算时间等的函数*/
+//找到最长直道的方向角等数据
+void get_straight_dire(Node* head);
 
-	double dst = get_distance(lat1, lng1, lat2, lng2);
-	return dst;
-}
- float get_diff_time(int a, int b) //a大b小
-{
-	 int minute = a / 1000 - b / 1000;
-	 int second = a / 10 % 100 - b / 10 % 100;
-	 int msecond = a % 10 - b % 10;
-	 if (second < 0)
-	 {
-		 second = second + 60;
-		 minute -= 1;
-	 }
-	 if (msecond < 0)
-	 {
-		 msecond = msecond + 10;
-		 second -= 1;
-	 }
-	 float time = float(minute * 60) + float(second) + float(msecond) / 10;
-	 return time;
-}
+//找到最长直道信息后，用此函数找出数据中符合方向角，经纬度要求的起点节点，后续可能要用此节点的时间等信息
+Node* get_start_point(Node *head);
+
+//判断当前起点是否越过了真正起点
+bool iscrossed(Node *p);
+
+//计算圈时,公式为（t2-delta_t2）-(t1-delata_t1),t1为上圈时间，delta_t1为上一圈插值
+float laptime(Node *last_start, Node* this_start);
+
+//判断当前节点是不是离起点距离最短的
+//abandoned(三次计算distance吃不消)
+void set_start(Node*lap, Node*new_circle);
+
+void update_dis(float&prior_dis, float&dis, float&next_dis, Node*p);
+
+float sum_dis(Node*a, Node*b);
+
+void update_mileage(float&seg_mileage, float&mileage, Node *p);
+
+//更新最快圈数据，包含加载数据，距离，时间，已经整体更新
+float* load_fast_speed(Node*start, Node*last, float fast[500], float lat[500], float lng[500]);
+
+
+/*最快圈时储存每两个节点之间的距离，后期如果优化计算负载可以在此函数内不用distance计算*/
+/*因为在计算总里程的时候更新过,但需要额外的空间来保存dis变量，以空间换时间*/
+float* load_fast_dis(Node*start, Node*last, float fast[500]);
+
+int* load_fast_time(Node*start, Node*last, int time[500]);
+
+void update_fast_lap(float lap_time, float &minlaptime, Node*last_start, Node*p);
+
+//dis为当前车走过的总距离，start_time_lap是本圈起始时的时间，now_seconds是当前时间
+float get_timediff(float dis, int start_time_lap, int now_seconds);
+
+
+float get_timediff1(float lat, float lng, int start_time_lap, int now_seconds, int &new_index);
+/*测试区代码，使用时需重写*/
+void load_data(float direction[5000], float lat[5000], float lng[5000], int time[5000], float speed[5000]);
 
  
